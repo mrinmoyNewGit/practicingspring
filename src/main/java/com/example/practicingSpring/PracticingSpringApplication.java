fee707a5eb4d979ac34023beab7f198a16fb5bbb
dev3 <dev3@gmail.com>
2025-06-11 12:42:49 +0530

    ist commit

---
+++ src/main/java/com/example/practicingSpring/PracticingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticingSpringApplication.class, args);
+	}
+
+}
