7e7d3f8e3f6ccf106621b3f59f22d243056c77b6
dev3 <dev3@gmail.com>
2025-06-11 15:45:58 +0530

    user push

---
+++ src/main/java/com/example/practicingSpring/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.practicingSpring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.practicingSpring.model.User;
+import com.example.practicingSpring.repository.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/example/practicingSpring/model/User.java
@@ -0,0 +1,18 @@
+package com.example.practicingSpring.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Document(collection = "users")
+@Data
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+
+}
