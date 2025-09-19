e512033ac9a85a6a7d984c1ec5e0d24fcbc32852
dev3 <dev3@gmail.com>
2025-09-19 12:36:57 +0530

    up config

--- src/main/java/com/example/practicingSpring/config/FilterConfig.java
+++ src/main/java/com/example/practicingSpring/config/FilterConfig.java
@@ -1,4 +1,4 @@
-package com.example.practicingSpring.security;
+package com.example.practicingSpring.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
